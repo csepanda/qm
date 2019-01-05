eff68d1ab406371906f64fc5b55c07435b365b2f
Andrey Bova <andrewbova@yandex.ru>
2019-01-05 07:41:56 +0300

    Add YAML-convertor for IPAddress

---
+++ src/parsers_ipv4address_yaml.cpp
@@ -0,0 +1,41 @@
+#include <yaml-cpp/yaml.h>
+#include <qm/model.hpp>
+#include <qm/parsers.hpp>
+
+namespace YAML {
+template<>
+struct convert<qm::parsers::yaml::IPAddressYamlDTO> {
+    static Node encode(const qm::parsers::yaml::IPAddressYamlDTO &addressDto) {
+        Node node;
+
+        switch (addressDto.ProtocolVersion) {
+            case qm::models::IPv4:
+                node = addressDto.IPv4.GetAddressStr();
+                break;
+            case qm::models::IPv6:
+                throw std::logic_error("Not implemented");
+        }
+
+        return node;
+    }
+
+    static bool decode(const Node &node, qm::parsers::yaml::IPAddressYamlDTO addressDto) {
+        const auto inputAddress = node.as<std::string>();
+        const auto protocolVersion = qm::parsers::determineIPVersion(inputAddress);
+
+        addressDto.ProtocolVersion = protocolVersion;
+        switch (protocolVersion) {
+            case qm::models::IPv4: {
+                const auto ipv4Data = qm::parsers::parseIPv4(inputAddress);
+                addressDto.IPv4.SetAddress(ipv4Data);
+                break;
+            }
+            case qm::models::IPv6:
+                throw std::logic_error("Not implemented");
+        }
+
+        return true;
+    }
+};
+}
+
