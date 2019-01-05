b8182122174b2f80bec76453a6f905015b36d6ab
Andrey Bova <andrewbova@yandex.ru>
2019-01-05 08:31:34 +0300

    Add new tests cases for yaml parsing

--- tests/src/parsers_ip_from_yaml.cpp
+++ tests/src/parsers_ip_from_yaml.cpp
@@ -12,11 +12,29 @@ broadcast: 255.255.255.255
 
     for (const auto& key : {"current", "loopback", "arbitrary", "broadcast"}) {
         SECTION(key) {
-            const auto inputNode = ipAddresses["current"];
+            const auto inputNode = ipAddresses[key];
             const auto actual = inputNode.as<qm::parsers::yaml::IPAddressYamlDTO>();
 
             REQUIRE(actual.ProtocolVersion == qm::models::IPv4);
             REQUIRE(actual.IPv4.GetAddressStr() == inputNode.as<std::string>());
         }
     }
+}
+
+TEST_CASE("try to parse IPv4 Address from invalid yaml", "[parsers][yaml]") {
+    const YAML::Node ipAddresses = YAML::Load(R"(
+current: 0..0.0
+loopback: 256.0.0.1
+arbitrary: foobar
+broadcast: 255.1024.255.255
+)");
+
+    for (const auto& key : {"current", "loopback", "arbitrary", "broadcast"}) {
+        SECTION(key) {
+            const auto inputNode = ipAddresses[key];
+
+            REQUIRE_THROWS_AS(inputNode.as<qm::parsers::yaml::IPAddressYamlDTO>(), qm::parsers::ParseException);
+        }
+    }
+
 }
