31c438d4fd10ded6faf7dd89161f1dfa3d18c16d
Andrey Bova <andrewbova@yandex.ru>
2019-01-05 07:31:57 +0300

    Implement qm::parsers::determineProtocolVersion

--- src/parsers.cpp
+++ src/parsers.cpp
@@ -10,6 +10,17 @@ namespace qm::parsers {
 static const std::regex ipv4Validator{R"(^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$)"};
 static const std::regex ipv6Validator{R"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"};
 
+
+qm::models::IPVersion determineProtocolVersion(const std::string &address) {
+    if (std::regex_match(address, ipv4Validator)) {
+        return qm::models::IPv4;
+    } else if (std::regex_match(address, ipv6Validator)) {
+        return qm::models::IPv6;
+    }
+
+    throw ParseException("Unknown IP address protocol version", address);
+}
+
 std::array<uint8_t, 4> parseIPv4(const std::string &address) {
     if (!std::regex_match(address, ipv4Validator)) {
         throw ParseException("doesn't match format: x.x.x.x", address);
