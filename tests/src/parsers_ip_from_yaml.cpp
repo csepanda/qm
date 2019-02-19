0b6fbf6cc286c0f70105d03414bbd870fd03d2c1
Andrey Bova <andrewbova@yandex.ru>
2019-02-19 22:54:55 +0300

    Add tests for yaml to IPv4 network parsing

--- tests/src/parsers_ip_from_yaml.cpp
+++ tests/src/parsers_ip_from_yaml.cpp
@@ -2,14 +2,36 @@
 #include <qm/parsers.hpp>
 #include <yaml-cpp/yaml.h>
 
-TEST_CASE("parse IPv4 Address from valid yaml", "[parsers][yaml]") {
-    const YAML::Node ipAddresses = YAML::Load(R"(
+static const auto validAddressesYaml = R"(
 current: 0.0.0.0
 loopback: 127.0.0.1
 arbitrary: 53.32.46.17
 broadcast: 255.255.255.255
-)");
+)";
 
+static const auto invalidAddressesYaml = R"(
+current: 0..0.0
+loopback: 256.0.0.1
+arbitrary: foobar
+broadcast: 255.1024.255.255
+)";
+
+static const auto validNetworksYaml = R"(
+current: 0.0.0.0/24
+loopback: 127.0.0.1/16
+arbitrary: 53.32.46.17/16
+broadcast: 255.255.255.255/0
+)";
+
+static const auto invalidNetworksYaml = R"(
+current: 0..0.0/24
+loopback: 256.0.0.1/16
+arbitrary: foobar/24
+broadcast: 255.1024.255.255/16
+)";
+
+TEST_CASE("parse IPv4 Address from valid yaml", "[parsers][yaml]") {
+    const YAML::Node ipAddresses = YAML::Load(validAddressesYaml);
     for (const auto& key : {"current", "loopback", "arbitrary", "broadcast"}) {
         SECTION(key) {
             const auto inputNode = ipAddresses[key];
@@ -22,12 +44,7 @@ broadcast: 255.255.255.255
 }
 
 TEST_CASE("try to parse IPv4 Address from invalid yaml", "[parsers][yaml]") {
-    const YAML::Node ipAddresses = YAML::Load(R"(
-current: 0..0.0
-loopback: 256.0.0.1
-arbitrary: foobar
-broadcast: 255.1024.255.255
-)");
+    const YAML::Node ipAddresses = YAML::Load(invalidAddressesYaml);
 
     for (const auto& key : {"current", "loopback", "arbitrary", "broadcast"}) {
         SECTION(key) {
@@ -36,5 +53,48 @@ broadcast: 255.1024.255.255
             REQUIRE_THROWS_AS(inputNode.as<qm::parsers::yaml::IPAddressYamlDTO>(), qm::parsers::ParseException);
         }
     }
+}
 
+TEST_CASE("parse IPv4 Network from valid yaml", "[parsers][yaml]") {
+    const YAML::Node ipAddresses = YAML::Load(validNetworksYaml);
+
+    for (const auto& key : {"current", "loopback", "arbitrary", "broadcast"}) {
+        SECTION(key) {
+            const auto inputNode = ipAddresses[key];
+            const auto actual = inputNode.as<qm::parsers::yaml::IPNetworkYamlDTO>();
+
+            REQUIRE(actual.ProtocolVersion == qm::models::IPv4);
+            REQUIRE(actual.IPv4.GetNetworkStr() == inputNode.as<std::string>());
+        }
+    }
+}
+
+TEST_CASE("try to parse IPv4 Network from invalid yaml", "[parsers][yaml]") {
+    const YAML::Node ipAddresses = YAML::Load(invalidNetworksYaml);
+
+    for (const auto& key : {"current", "loopback", "arbitrary", "broadcast"}) {
+        SECTION(key) {
+            const auto inputNode = ipAddresses[key];
+
+            REQUIRE_THROWS_AS(inputNode.as<qm::parsers::yaml::IPNetworkYamlDTO>(), qm::parsers::ParseException);
+        }
+    }
+}
+
+TEST_CASE("parse IPv4 Network from invalid format string", "[parsers][yaml]") {
+    const YAML::Node yaml = YAML::Load("192.168.0.1");
+
+    REQUIRE_THROWS_AS(yaml.as<qm::parsers::yaml::IPNetworkYamlDTO>(), qm::parsers::ParseException);
+}
+
+TEST_CASE("parse IPv4 Network from string with invalid cidr value", "[parsers][yaml]") {
+    const YAML::Node yaml = YAML::Load("192.168.0.1/64");
+
+    REQUIRE_THROWS_AS(yaml.as<qm::parsers::yaml::IPNetworkYamlDTO>(), std::invalid_argument);
+}
+
+TEST_CASE("parse IPv4 Network from string with invalid cidr", "[parsers][yaml]") {
+    const YAML::Node yaml = YAML::Load("192.168.0.1/dasd");
+
+    REQUIRE_THROWS_AS(yaml.as<qm::parsers::yaml::IPNetworkYamlDTO>(), qm::parsers::ParseException);
 }
