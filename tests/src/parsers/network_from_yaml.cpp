1d19f58b8e5f173e7126c5aea018d5bf1ed58095
Andrey Bova <andrewbova@yandex.ru>
2019-02-20 14:33:28 +0300

    Add reference resolving tests for network yaml-parsing

--- tests/src/parsers/network_from_yaml.cpp
+++ tests/src/parsers/network_from_yaml.cpp
@@ -4,39 +4,76 @@
 #include <catch2/catch.hpp>
 #include <qm/parsers.hpp>
 
-TEST_CASE("parse Network from valid yaml", "[parsers][yaml]") {
-    const YAML::Node yaml = YAML::Load(R"(
+static const std::string validYamlWithIdsAndReferences = R"(
 nodes:
   - id: node01
     ipConfig:
       - address: 10.0.1.3/24
-        connection: !Ref connection_01
+        connection: !Ref connection01
   - id: node02
     ipConfig:
       - address: 10.0.1.4/24
-        connection: !Ref connection_01
+        connection: !Ref connection01
 connections:
   - id: connection01
     type: P2P
-    target:
+    targets:
       - !Ref node01
       - !Ref node02
     dataRate: 5Mbps
     delay: 1ms
     mtu: 1500
-)");
+)";
 
+TEST_CASE("parse Network from valid yaml // correct nodes filling", "[parsers][yaml]") {
+    const YAML::Node yaml = YAML::Load(validYamlWithIdsAndReferences);
     const auto actual = yaml.as<qm::parsers::yaml::NetworkYamlDTO>();
 
-    SECTION("ASSERT NODES") {
-        REQUIRE(actual.NodesDTO.size() == 2);
-        REQUIRE(actual.NodesDTO.at("node01").GetId() == "node01");
-        REQUIRE(actual.NodesDTO.at("node02").GetId() == "node02");
-    }
+    REQUIRE(actual.NodesDTO.size() == 2);
+    REQUIRE(actual.NodesDTO.at("node01").GetId() == "node01");
+    REQUIRE(actual.NodesDTO.at("node02").GetId() == "node02");
+}
 
-    SECTION("ASSERT CONNECTIONS") {
-        REQUIRE(actual.ConnectionsDTO.size() == 1);
-        REQUIRE(actual.ConnectionsDTO.at("connection01").GetId() == "connection01");
+TEST_CASE("parse Network from valid yaml // correct connections filling", "[parsers][yaml]") {
+    const YAML::Node yaml = YAML::Load(validYamlWithIdsAndReferences);
+    const auto actual = yaml.as<qm::parsers::yaml::NetworkYamlDTO>();
+
+    REQUIRE(actual.ConnectionsDTO.size() == 1);
+    REQUIRE(actual.ConnectionsDTO.at("connection01").GetId() == "connection01");
+}
+
+TEST_CASE("parse Network from valid yaml // references resolving in nodes", "[parsers][yaml]") {
+    const YAML::Node yaml = YAML::Load(validYamlWithIdsAndReferences);
+    const auto actual = yaml.as<qm::parsers::yaml::NetworkYamlDTO>();
+
+    for (const auto&[id, node] : actual.NodesDTO) {
+        const qm::parsers::yaml::NodeYamlDTO n = node;
+
+        for (const auto& ipConfig : n.Node->GetIpConfigs()) {
+            REQUIRE(ipConfig.BindConnection != nullptr);
+        }
+
+        for (const auto &ipConfigDTO : n.IpConfigs) {
+            REQUIRE(ipConfigDTO.ConnectionRef.Resolved);
+        }
+    }
+}
+
+TEST_CASE("parse Network from valid yaml // references resolving in connections", "[parsers][yaml]") {
+    const YAML::Node yaml = YAML::Load(validYamlWithIdsAndReferences);
+    const auto actual = yaml.as<qm::parsers::yaml::NetworkYamlDTO>();
+
+    for (const auto&[id, connection] : actual.ConnectionsDTO) {
+        const auto nodes = connection.GetConnection()->GetNodes();
+
+        REQUIRE(!nodes.empty());
+        for (const auto& node : nodes) {
+            REQUIRE(node != nullptr);
+        }
+
+        for (const auto &ref : connection.TargetsRefs) {
+            REQUIRE(ref.Resolved);
+        }
     }
 }
 
@@ -44,9 +81,6 @@ TEST_CASE("parse Network from valid yaml // generate missing id", "[parsers][yam
     const YAML::Node yaml = YAML::Load(R"(
 connections:
   - type: P2P
-    target:
-      - !Ref node01
-      - !Ref node02
     dataRate: 5Mbps
     delay: 1ms
     mtu: 1500
