528babf5226151db99aa91500063d22c94ee2c57
Andrey Bova <andrewbova@yandex.ru>
2019-02-20 13:49:45 +0300

    Add missing id generation test for network cfg parsing

--- tests/src/parsers/network_from_yaml.cpp
+++ tests/src/parsers/network_from_yaml.cpp
@@ -1,3 +1,6 @@
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/lexical_cast.hpp>
 #include <catch2/catch.hpp>
 #include <qm/parsers.hpp>
 
@@ -34,5 +37,32 @@ connections:
     SECTION("ASSERT CONNECTIONS") {
         REQUIRE(actual.ConnectionsDTO.size() == 1);
         REQUIRE(actual.ConnectionsDTO.at("connection01").GetId() == "connection01");
+    }
+}
+
+TEST_CASE("parse Network from valid yaml // generate missing id", "[parsers][yaml]") {
+    const YAML::Node yaml = YAML::Load(R"(
+connections:
+  - type: P2P
+    target:
+      - !Ref node01
+      - !Ref node02
+    dataRate: 5Mbps
+    delay: 1ms
+    mtu: 1500
+)");
+
+    const auto actual = yaml.as<qm::parsers::yaml::NetworkYamlDTO>();
+
+    REQUIRE(actual.ConnectionsDTO.size() == 1);
+
+    for (const auto& [key, connection] : actual.ConnectionsDTO) {
+        const std::string id = connection.GetId();
+
+        REQUIRE(key == id);
+
+        boost::uuids::uuid uuid{};
+
+        REQUIRE_NOTHROW(uuid = boost::lexical_cast<boost::uuids::uuid, std::string>(id));
     }
 }
