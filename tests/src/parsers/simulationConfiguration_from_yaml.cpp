ece5808e0090d81afbbbd37841a67adf8c40d256
Andrey Bova <andrewbova@yandex.ru>
2019-03-03 21:46:27 +0300

    Add tests for simulationConfiguration_from_yaml parsing

---
+++ tests/src/parsers/simulationConfiguration_from_yaml.cpp
@@ -0,0 +1,37 @@
+#include <catch2/catch.hpp>
+#include <qm/parsers.hpp>
+#include <yaml-cpp/yaml.h>
+#include <qm/yaml/dto/SimulationConfigurationYamlDTO.hpp>
+
+std::string createSCValidYaml(const std::string &networkStack);
+std::unordered_map<std::string, qm::models::NetworkStack> networkStacksDict = {
+  {"linux", qm::models::NetworkStack::LinuxKernel},
+  {"ns3", qm::models::NetworkStack::Ns3}
+};
+
+TEST_CASE("parse SimulationConfiguration from valid yaml", "[parsers][yaml]") {
+    const std::vector<std::string> networkStacks = {"linux", "ns3"};
+
+    for (const auto& ns : networkStacks) {
+        const YAML::Node yaml = YAML::Load(createSCValidYaml(ns));
+        const auto actual = yaml.as<qm::parsers::yaml::SimulationConfigurationYamlDTO>();
+
+        REQUIRE(actual.networkStack == networkStacksDict.at(ns));
+    }
+}
+
+TEST_CASE("parse SimulationConfiguration from invalid yaml", "[parsers][yaml]") {
+    const YAML::Node yaml = YAML::Load(R"(
+networkStack: foo-bar
+)");
+    REQUIRE_THROWS(yaml.as<qm::parsers::yaml::SimulationConfigurationYamlDTO>());
+}
+
+std::string createSCValidYaml(const std::string &networkStack) {
+    std::stringstream ss;
+
+    ss << "networkStack: " << networkStack << "\n";
+
+    return ss.str();
+}
+
