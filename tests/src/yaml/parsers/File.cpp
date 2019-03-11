50edd60e1a2776b59045f91ae2d956b2401d0af4
Andrey Bova <andrewbova@yandex.ru>
2019-03-11 21:07:54 +0300

    Update File parsing tests

--- tests/src/yaml/parsers/File.cpp
+++ tests/src/yaml/parsers/File.cpp
@@ -2,7 +2,7 @@
 #include <qm/parsers.hpp>
 
 // TODO update tests
-TEST_CASE("parse file from valid yaml", "[parsers][yaml]") {
+TEST_CASE("File parsing. Valid yaml", "[parsers][yaml]") {
     const YAML::Node yaml = YAML::Load(R"(
 path: /etc/foo/bar.cfg
 text: |
@@ -12,9 +12,32 @@ text: |
 
     const auto actual = yaml.as<qm::yaml::dto::File>();
 
-    REQUIRE(actual.Type == qm::models::FileType::Regular);
-    REQUIRE(actual.RegularSourceType == qm::yaml::dto::RegularFileSourceType::Text);
-    REQUIRE(actual.Path == "/etc/foo/bar.cfg");
-    REQUIRE(actual.TextContent == "debug=true\nlogLevel=verbose\n");
+    SECTION("correct implicitly parsed type is set") {
+        REQUIRE(actual.Type == qm::models::FileType::Regular);
+    }
+
+    SECTION("source of regular file is correctly determined by parser") {
+        REQUIRE(actual.RegularSourceType == qm::yaml::dto::RegularFileSourceType::Text);
+    }
+
+    SECTION("path is parsed correctly") {
+        REQUIRE(actual.Path == "/etc/foo/bar.cfg");
+    }
+
+    SECTION("Multiline text content is parsed correctly") {
+        REQUIRE(actual.TextContent == "debug=true\nlogLevel=verbose\n");
+    }
+}
+
+TEST_CASE("File parsing. Invalid Yaml") {
+    SECTION("Path is missing") {
+        const YAML::Node yaml = YAML::Load(R"(
+text: |
+  debug=true
+  logLevel=verbose
+)");
+
+        REQUIRE_THROWS(yaml.as<qm::yaml::dto::File>());
+    }
 }
 
