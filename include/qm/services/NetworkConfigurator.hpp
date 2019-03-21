f42ea5eb2da738a727bd6145a5297922de81a5d1
Andrey Bova <andrewbova@yandex.ru>
2019-03-21 03:04:29 +0300

    Update NetworkConfigurator to use Ns3NodeCreator service

--- include/qm/services/NetworkConfigurator.hpp
+++ include/qm/services/NetworkConfigurator.hpp
@@ -1,25 +1,26 @@
-//
-// Created by cspanda on 3/3/19.
-//
-
 #ifndef QM_NETWORKCONFIGURATOR_H
 #define QM_NETWORKCONFIGURATOR_H
 
 #include "IpConfigurator.hpp"
+#include "INs3NodeCreator.hpp"
 
 namespace qm::services {
 class NetworkConfigurator {
-    std::unique_ptr<qm::services::IpConfigurator> m_ipConfigurator;
+    std::unique_ptr<IpConfigurator> m_ipConfigurator;
+    std::unique_ptr<INs3NodeCreator> m_ns3NodeCreator;
 
-    void ConfigureP2PConnection(const qm::models::Network& network, const std::shared_ptr<qm::models::PointToPointConnection> &connection) const;
+    void ConfigureP2PConnection(
+      const qm::models::Network &network,
+      const std::shared_ptr<qm::models::PointToPointConnection> &connection) const;
 public:
     NetworkConfigurator(
       qm::models::NetworkStack &,
+      std::unique_ptr<INs3NodeCreator> nodeCreator,
       std::shared_ptr<TimeSequence>,
       std::shared_ptr<ns3::DceManagerHelper>
     );
 
-    void Configure(qm::models::Network& network) const;
+    void Configure(qm::models::Network &network) const;
 };
 }
 
--- src/services/NetworkConfigurator.cpp
+++ src/services/NetworkConfigurator.cpp
@@ -5,15 +5,17 @@
 #include <qm/services/NetworkConfigurator.hpp>
 #include <qm/exceptions.hpp>
 #include <ns3/point-to-point-helper.h>
+#include <qm/services/SimpleNs3NodeCreator.hpp>
+#include <qm/services/mpi/ManualSystemIdMarkerStrategy.hpp>
 
 namespace qm::services {
 
 NetworkConfigurator::NetworkConfigurator(
   qm::models::NetworkStack &networkStack,
+  std::unique_ptr<INs3NodeCreator> nodeCreator,
   std::shared_ptr<TimeSequence> timer,
   std::shared_ptr<ns3::DceManagerHelper> dceManager
 ) {
-
     switch (networkStack) {
         case models::NetworkStack::Ns3:
             m_ipConfigurator = std::make_unique<Ns3StackIpConfigurator>();
@@ -22,13 +24,14 @@ NetworkConfigurator::NetworkConfigurator(
             m_ipConfigurator = std::make_unique<qm::services::dce::LinuxStackIpConfigurator>(timer, dceManager);
             break;
     }
+
+    m_ns3NodeCreator = std::move(nodeCreator);
 }
 
 inline static const auto NC_CONFIGURE_SOURCE = "NetworkConfigurator::Configure";
+
 void NetworkConfigurator::Configure(qm::models::Network &network) const {
-    for (const auto &node : network.GetNodes()) {
-        node->SetNS3Node(ns3::CreateObject<ns3::Node>());
-    }
+    m_ns3NodeCreator->Create(network);
 
     for (const auto &connection : network.GetConnections()) {
         switch (connection->GetConnectionType()) {
@@ -47,6 +50,7 @@ void NetworkConfigurator::Configure(qm::models::Network &network) const {
 }
 
 inline static const auto NC_P2P_SOURCE = "NetworkConfigurator::ConfigureP2PConnection";
+
 void NetworkConfigurator::ConfigureP2PConnection(
   const qm::models::Network &network,
   const std::shared_ptr<qm::models::PointToPointConnection> &connection
@@ -80,6 +84,6 @@ void NetworkConfigurator::ConfigureP2PConnection(
         ipConfig->NS3NetDevice = netDevice;
     }
 
-    helper.EnablePcapAll ("simulation-p2p");
+    // helper.EnablePcapAll("simulation-p2p");
 }
 };
