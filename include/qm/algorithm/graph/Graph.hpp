8deed66f2dae7a25432f2a02fdf34e8ec5531963
Andrey Bova <andrewbova@yandex.ru>
2019-04-01 06:41:53 +0300

    Add verticesIds map to Graph

--- include/qm/algorithm/graph/Graph.hpp
+++ include/qm/algorithm/graph/Graph.hpp
@@ -1,14 +1,17 @@
 #ifndef QM_ALGORITHM_GRAPH_GRAPH_HPP
 #define QM_ALGORITHM_GRAPH_GRAPH_HPP
 
+#include <map>
 #include <memory>
+#include <vector>
 #include "Vertex.hpp"
 
 namespace qm::algorithm::graph {
 template<typename TNode, typename TEdge>
 class Graph {
-    std::vector<std::shared_ptr<Vertex<TNode, TEdge>>> m_vertices;
-    std::vector<std::shared_ptr<Edge<TNode, TEdge>>> m_edges;
+    const std::vector<std::shared_ptr<Vertex<TNode, TEdge>>> m_vertices;
+    const std::vector<std::shared_ptr<Edge<TNode, TEdge>>> m_edges;
+    const std::map<std::shared_ptr<Vertex<TNode, TEdge>>, uint32_t> m_verticesIds;
 public:
     Graph(
       std::vector<std::shared_ptr<Vertex<TNode, TEdge>>> vertices,
@@ -18,6 +21,10 @@ public:
     const std::vector<std::shared_ptr<Vertex<TNode, TEdge>>> &GetVertices() const;
 
     const std::vector<std::shared_ptr<Edge<TNode, TEdge>>> &GetEdges() const;
+
+    const uint32_t GetVertexId(const std::shared_ptr<Vertex<TNode, TEdge>> &) const;
+
+    const std::shared_ptr<Vertex<TNode, TEdge>> &GetVertexById(const uint32_t id) const;
 };
 }
 
--- src/algorithm/graph/Graph.cpp
+++ src/algorithm/graph/Graph.cpp
@@ -9,6 +9,13 @@ Graph<TNode, TEdge>::Graph(
   : m_vertices{vertices},
     m_edges{edges} {
 
+    std::map<std::shared_ptr<Vertex<TNode, TEdge>>, uint32_t> verticesIds;
+
+    for (auto i = 0; i < vertices.size(); i++) {
+        verticesIds[vertices[i]] = i;
+    }
+
+    m_verticesIds = verticesIds;
 }
 
 template<typename TNode, typename TEdge>
@@ -19,5 +26,15 @@ const std::vector<std::shared_ptr<Vertex<TNode, TEdge>>> &Graph<TNode, TEdge>::G
 template<typename TNode, typename TEdge>
 const std::vector<std::shared_ptr<Edge<TNode, TEdge>>> &Graph<TNode, TEdge>::GetEdges() const {
     return m_edges;
+}
+
+template<typename TNode, typename TEdge>
+const uint32_t Graph<TNode, TEdge>::GetVertexId(const std::shared_ptr<Vertex<TNode, TEdge>> &vertex) const {
+    return m_verticesIds[vertex];
+}
+
+template<typename TNode, typename TEdge>
+const std::shared_ptr<Vertex<TNode, TEdge>> &Graph<TNode, TEdge>::GetVertexById(const uint32_t id) const {
+    return m_vertices[id];
 }
 }
