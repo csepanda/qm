7e5fe5f9c659138b9b39720e52971773284a1e16
Andrey Bova <andrewbova@yandex.ru>
2019-05-10 04:46:14 +0300

    Update ApplicationInstaller to handle new TimeSequence API

--- src/services/ApplicationInstaller.cpp
+++ src/services/ApplicationInstaller.cpp
@@ -26,20 +26,24 @@ void ApplicationInstaller::Install(const std::shared_ptr<models::Process> &proce
 
     auto appContainer = applicationHelper.Install(process->GetNode()->GetNS3Node());
     auto time = process->GetStartTime();
+    auto nodeId = process->GetNode()->GetId();
 
     if (time == 0) {
-        time = m_timer->NextSeconds();
+        time = m_timer->NextSeconds(nodeId);
     } else if (m_timer->CurrentSeconds().ToDouble(ns3::Time::Unit::S) < time.ToDouble(ns3::Time::Unit::S)) {
-        m_timer->SetSeconds(time);
+        m_timer->SetSeconds(time, nodeId);
     }
 
     appContainer.Start(time);
 }
 
 void ApplicationInstaller::Install(const std::vector<std::shared_ptr<qm::models::Process>> &processes) {
+    m_timer->SetStep(0.01);
+
     for (const auto &process : processes) {
         Install(process);
     }
+    m_timer->ResetStep();
 }
 
 }
