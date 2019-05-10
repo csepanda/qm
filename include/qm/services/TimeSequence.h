6377bd18745437bfa733ad327091b2ec46311bd4
Andrey Bova <andrewbova@yandex.ru>
2019-05-10 04:47:41 +0300

    Update TimeSequence header file to new api

--- include/qm/services/TimeSequence.h
+++ include/qm/services/TimeSequence.h
@@ -2,22 +2,38 @@
 #define QM_SERVICES_TIMESEQUENCE_H
 
 #include <ns3/nstime.h>
+#include <map>
 
 namespace qm::services {
 class TimeSequence {
+    std::map<std::string, double> m_timers;
     double m_time;
-public:
+    double m_step;
+
     double Next();
-
-    ns3::Time NextSeconds();
-
-    ns3::Time CurrentSeconds();
 
     double Next(double skip);
 
     double Current() const;
 
+public:
+    TimeSequence();
+
+    ns3::Time NextSeconds();
+
+    ns3::Time NextSeconds(const std::string &nodeId);
+
+    ns3::Time CurrentSeconds();
+
+    ns3::Time CurrentSeconds(const std::string &nodeId);
+
     void SetSeconds(ns3::Time value);
+
+    void SetSeconds(ns3::Time value, const std::string &nodeId);
+
+    void SetStep(double step);
+
+    void ResetStep();
 };
 }
 
